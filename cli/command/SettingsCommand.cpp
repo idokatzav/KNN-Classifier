4608ea84fee470babc884401d3cf7605a20c4d7f
idok2 <ido.k2005@gmail.com>
2022-08-28 10:39:17 +0300

    minor changes in SettingsCommand

--- cli/command/SettingsCommand.cpp
+++ cli/command/SettingsCommand.cpp
@@ -1,5 +1,5 @@
 #include "SettingsCommand.h"
 void SettingsCommand::execute() {
     m_dio->write("The current KNN parameters are: K = " + std::to_string(m_classifierData->classifier().k())
-                + ", distance metric = " + m_classifierData->metric());
+                + ", distance metric = " + m_classifierData->metric().name());
 }
